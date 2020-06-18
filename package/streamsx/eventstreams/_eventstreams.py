ea82312c3279dbe7981c334be47e8d09b487d258
markheger <mark-oliver.heger@de.ibm.com>
2020-06-18 14:44:21 +0000

    description updated, resolves #18

--- package/streamsx/eventstreams/_eventstreams.py
+++ package/streamsx/eventstreams/_eventstreams.py
@@ -91,15 +91,14 @@ def configure_connection(instance, name='eventstreams', credentials=None):
 
     Example for creating a configuration for a Streams instance with connection details::
 
-
-        from streamsx.rest import Instance
-        import streamsx.topology.context
         from icpd_core import icpd_util
+        from streamsx.rest_primitives import Instance
+        import streamsx.eventstreams as es
 
         cfg = icpd_util.get_service_instance_details(name='your-streams-instance')
         cfg[streamsx.topology.context.ConfigParams.SSL_VERIFY] = False
         instance = Instance.of_service(cfg)
-        app_cfg = configure_connection(instance, credentials='my_crdentials_json')
+        app_cfg = es.configure_connection(instance, credentials='my_crdentials_json')
 
 
     Args:
